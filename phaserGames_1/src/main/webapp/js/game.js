c3fbe2a918d5be400e10a4779cc611dd88ec5ddf
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-12-15 22:13:08 +0100

    init game_1 with the official tutorial

---
+++ phaserGames_1/src/main/webapp/js/game.js
@@ -0,0 +1,100 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+
+function preload() {
+    game.load.image('star', 'assets/star.png');
+    game.load.image('platform', 'assets/platform.png');
+    game.load.image('sky', 'assets/sky.png');
+    game.load.spritesheet('character', 'assets/dude.png', 32, 48);
+}
+
+var platforms;
+var player;
+var stars;
+var score = 0;
+var scoreText;
+var life = 10;
+var lifeText;
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    var sky = game.add.sprite(0, 0, 'sky');
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    var ground = platforms.create(0, game.world.height - 64, 'platform');
+    ground.scale.setTo(2, 2, 25);
+    ground.body.immovable = true;
+    var ledge = platforms.create(400, 400, 'platform');
+    ledge.body.immovable = true;
+    ledge = platforms.create(-150, 250, 'platform');
+    ledge.body.immovable = true;
+    player = game.add.sprite(32, game.world.height - 150, 'character');
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.0;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    scoreText = game.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#000'});
+    lifeText = game.add.text(game.world.height - 50, 16, 'Life: 100', {fontSize: '32px', fill: '#000'});
+    createStars();
+}
+
+function createStars() {
+    stars = game.add.group();
+    stars.enableBody = true;
+    var star = stars.create(20, 40, 'star');
+//    star.body.immovable = true;
+}
+
+function update() {
+    var hitPlatform = game.physics.arcade.collide(player, platforms);
+    keyControls(hitPlatform);
+    game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    if(life === 0){
+        game.state.start('Over');
+    }
+}
+
+function collectStar(player, star) {
+    score += 10;
+    scoreText.text = "Score : " + score;
+    star.kill();
+}
+
+function jump(){
+        life -= 10;
+        lifeText.text = "Life : " + life;
+}
+
+function keyControls(hitPlatform) {
+    cursors = game.input.keyboard.createCursorKeys();
+    player.body.velocity.x = 0;
+    if (cursors.left.isDown) {
+        //  Move to the left
+        player.body.velocity.x = -350;
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown) {
+        //  Move to the right
+        player.body.velocity.x = 350;
+        player.animations.play('right');
+    }
+    else {
+        //  Stand still
+        player.animations.stop();
+        player.frame = 4;
+    }
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
+        jump();
+        player.body.velocity.y = -350;
+    }
+}
